3adc9ba47fe8d5ea940dadda628eb9adfb9fbca4
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2024-01-23 15:18:23 -0800

    minor changes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-function Gameboard() {
+const Gameboard = (function () {
     const board = [
         [0, 0, 0],
         [0, 0, 0],
@@ -16,12 +16,38 @@ function Gameboard() {
     const printBoard = () => {
         console.table(board);
     };
-
+    
     return { getBoard, markSquare, printBoard };
-}
+    
+})();
 
 /* 
 ** 0: empty
 ** 1: player 1 "x"
 ** 2: player 2 "o"
+*/
+
+/*
+function game() {
+    create player 1
+    create player 2
+
+    turn tracking:
+        let round = 1
+        round++ after each turn
+        return odd or even
+
+    check for a winner:
+        // what about draws??
+        
+
+    while no winner { 
+        
+        if turn is odd, prompt player 1 for a row and column
+        if turn is even, prompt player 2 for a row and column
+    }
+
+
+}
+
 */
